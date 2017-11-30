2b19bec1f913cc27b9ced222e0f7fc1424adc7be
Volodymyr Naydonov <vvpodufalov@gmail.com>
2017-11-30 13:15:42 +0200

    no message

---
+++ ex39_3.py
@@ -0,0 +1,26 @@
+days ={
+'Mo': 'Monday',
+'Tue': 'Tuesday',
+'Wed': 'Wednesday',
+'Thu': 'Thursday',
+'Fri': 'Friday'
+}
+
+# add days
+days['Sat'] = 'Saturday'
+days['Sun'] = 'Sunday'
+
+print('Here is week : ', days)
+
+count_days = {
+'Monday': 1,
+'Tuesday': 2,
+'Wednesday': 3,
+'Thursday': 4,
+'Friday': 5,
+'Saturday': 6,
+'Sunday': 7
+}
+
+print(f'The second day in a weekend is {days["Tue"]}')
+print(f'Tuesday is the number {count_days[days["Tue"]]} in a week')
