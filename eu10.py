edc83eb816586e1bdba33681eedf190333dc4736
Volodymyr Naydonov <vvpodufalov@gmail.com>
2017-12-26 11:38:21 +0200

    no message

--- eu10.py
+++ eu10.py
@@ -3,16 +3,16 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
 '''
-L = 21
-z =2
-f = [i for i in range(0, L)]
+L = 20
+
+f = [i for i in range(0, int(L + 1))]
 
 for x in f[2:]:
     if f[x] != 0:
-        n = z
-        print ("excluding all  numbers // {}".format(x))
-        while x*n < L:
+       n = 2
+       print ("excluding all  numbers // {}".format(x))
+       while x*n < L:
             f[x*n] = 0
-            n+= 1
+            n+=1
 
 print(sum([i for i in f if i !=0]))
--- eu10_2.py
+++ eu10_2.py
@@ -1,3 +1,4 @@
+'''
 n = 21
 a = [True] * n
 
@@ -10,3 +11,19 @@ for k in range (2, n):
             a[m] = False
 for k in range(n):
     print(k, '-','prostoe' if a[k] else 'sostavnoe')
+'''
+number = 1000 + 1
+def mult (number):
+    natural = []
+    for x in range (1, number):
+        if x % 3 == 0:
+            natural.append(x)
+        elif x % 5 == 0:
+            natural.append(x)
+        else:
+            pass
+    return natural
+
+print(mult(number))
+sum_it = mult(number)
+print(sum(sum_it))
